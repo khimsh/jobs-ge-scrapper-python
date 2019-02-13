956582731afb46e4d378afc33e2b5361c2706a04
khimsh <i.khimsh@gmail.com>
2019-02-13 16:06:59 +0400

    minor cleanup of the code

--- scrapper.py
+++ scrapper.py
@@ -1,25 +1,45 @@
+# scrapper.py
+
+import re
+import time
+from csv import writer
+
 import requests
 from bs4 import BeautifulSoup
 import lxml
-import re
-from csv import writer
 
-'''
+import config
+
+from functions import open_ad
+from functions import get_page_count
+from functions import add_property
+
+
+start = time.time()
+
+"""
 Collect All URLs
-'''
-xml = requests.get('http://www.jobs.ge/rss/').content
+"""
+entry_point = config.config['url']
+
+try:
+    xml = requests.get(entry_point, timeout=3).content
+except ConnectionError:
+    raise ConnectionError
 
 raw_urls = BeautifulSoup(xml, 'lxml').find_all('guid')
 
-urls = []
 
+# Collect all advertisment URLs in urls list
+urls = []
 for raw_url in raw_urls:
     urls.append(raw_url.text)
+print('All links collected.')
 
 
-'''
+"""
 Collect category URLs
-'''
+"""
 categories = ['admin', 'sales', 'finance', 'prmarketing',
               'technical', 'it', 'law', 'healthcare', 'other']
 
@@ -39,83 +59,99 @@ category_list = {
 
 for category in categories:
 
-    html = requests.get(
-        f'http://jobs.ge/?page=1&keyword=&cat={category}&location=&view=').content
+    try:
+        html = requests.get(
+            f'http://jobs.ge/?page=1&keyword=&cat={category}&location=&view=', timeout=3).content
+    except ConnectionError:
+        raise ConnectionError
+
     bs = BeautifulSoup(html, 'lxml')
-    links = bs.find_all('a', {'class': 'ls'}, href=re.compile('\/[0-9]+\/'))
+
+    page_count = get_page_count(bs)
+
+    links = list()
+
+    if page_count > 1:
+        for page in range(1, page_count + 1):
+            html = requests.get(
+                f'http://jobs.ge/?page={page}&keyword=&cat={category}&location=&view=').content
+            bs = BeautifulSoup(html, 'lxml')
+            links += bs.find_all('a', {'class': 'ls'},
+                                 href=re.compile(r'\/[0-9]+\/'))
+    else:
+        links += bs.find_all('a', {'class': 'ls'},
+                             href=re.compile(r'\/[0-9]+\/'))
 
     for link in links:
         category_list[category].append(link.attrs['href'])
 
-# Dictionay for a single ad
-advertisment = dict()
 
+"""
+Collect advertisment types URLs
+"""
+advertisment_types = ['jobs', 'scholarships', 'trainings', 'tenders', 'other']
 
-def openAd(url):
-    """
-    Access advertisment link and scrap the content
-    1. Open advertisment
-    2. Get all <tr> that containt advertisment information
-    3. Construct and return Advertisment Dictionary
-    """
+advertisment_type_list = {
+    'jobs': [],
+    'scholarships': [],
+    'trainings': [],
+    'tenders': [],
+    'other': []
+}
 
-    global advertisment
+for advertisment_type in advertisment_types:
 
-    # List to store needed <b> tags
-    ad_list = list()
+    html = requests.get(
+        f'http://jobs.ge/?page=1&keyword=&cat=&location=&view={advertisment_type}').content
+    bs = BeautifulSoup(html, 'lxml')
 
-    html = requests.get(url).content
+    page_count = get_page_count(bs)
 
-    # Get all <tr> inside <table class="ad">
-    ad_trs = BeautifulSoup(html, 'lxml').find(
-        'table', {'class': 'ad'}).find_all('tr')
+    links = list()
 
-    # Get advertisment text from returned table rows
-    # Strip \n\r\t from advertisment text
-    ad_text = re.sub(r'[\t\r\n]', '', ad_trs.pop().text)
+    if page_count > 1:
+        for page in range(1, page_count + 1):
+            html = requests.get(
+                f'http://jobs.ge/?page={page}&keyword=&cat=&location=&view={advertisment_type}').content
+            bs = BeautifulSoup(html, 'lxml')
+            links += bs.find_all('a', {'class': 'ls'},
+                                 href=re.compile(r'\/[0-9]+\/'))
+    else:
+        links += bs.find_all('a', {'class': 'ls'},
+                             href=re.compile(r'\/[0-9]+\/'))
 
-    # Iterate over ad_trs and append <b> tags to ad_list list
-    for tr in ad_trs:
-        ad_properties = tr.find_all('b')
-        ad_list.append(ad_properties)
-
-    # assign properties to ad dictionary
-    advertisment['title'] = ad_list[0][0].text
-    advertisment['company'] = ad_list[1][0].text
-    advertisment['post_date'] = ad_list[2][0].text
-    advertisment['final_date'] = ad_list[2][1].text
-    advertisment['text'] = ad_text
-    advertisment['lang'] = 'ქართული'
-    advertisment['relative_url'] = url[18:]
-    advertisment['absolute_url'] = url
-    advertisment['translation_url'] = 'http://jobs.ge/eng' + url[18:]
-
-    for key, values in category_list.items():
-        for value in values:
-            if value == url[18:]:
-                advertisment['category'] = key
-
-    return advertisment
+    for link in links:
+        advertisment_type_list[advertisment_type].append(link.attrs['href'])
 
 
 # Write CSV file from scrapped data
 with open('advertisments.csv', 'w', encoding="utf-8") as csv_file:
     csv_writer = writer(csv_file)
-    headers = ['title',
-               'post_date',
-               'final_date',
-               'category',
-               'company',
-               'lang',
-               'translation',
-               'ad_text',
-               'absolute_url',
-               'relative_url']
+    headers = ['სათაური',
+               'განცხადების ტიპი',
+               'გამოქვეყნების თარიღი',
+               'საბოლოო თარიღი',
+               'კატეგორია',
+               'კომპანია',
+               'განცხადების ენა',
+               'თარგმანის ლინკი',
+               'განცხადება',
+               'Absolute URL',
+               'Relative URL']
     csv_writer.writerow(headers)
 
+    advertisment = dict()
     for url in urls:
-        openAd(url)
+        open_ad(advertisment, url)
+
+        add_property(advertisment, category_list, 'category',
+                     advertisment['absolute_url'])
+
+        add_property(advertisment,
+                     advertisment_type_list, 'type', advertisment['absolute_url'])
+
         csv_writer.writerow([advertisment['title'],
+                             advertisment['type'],
                              advertisment['post_date'],
                              advertisment['final_date'],
                              advertisment['category'],
@@ -126,4 +162,7 @@ with open('advertisments.csv', 'w', encoding="utf-8") as csv_file:
                              advertisment['absolute_url'],
                              advertisment['relative_url']
                              ])
-        print('added to csv file')
+
+end = time.time()
+
+print((end - start) // 60)
